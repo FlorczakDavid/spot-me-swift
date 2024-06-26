c8185a3400ec8abef8b4c65e655a5d2cbcffa435
David FLORCZAK <florczakdavid@gmail.com>
2024-06-26 12:44:34 +0200

    Initial Commit

---
+++ Spot-me/Spot_meApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spot_meApp.swift
+//  Spot-me
+//
+//  Created by david florczak on 26/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Spot_meApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
